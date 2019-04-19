6b778c7a69a6d2934ff79cf35b78e853dc648699
yaqing <522675473@qq.com>
2019-04-19 12:33:39 +0800

    add wifi controller.

---
+++ app/src/main/java/com/example/ts/safetyguard/controller/WifiController.java
@@ -0,0 +1,64 @@
+package com.example.ts.safetyguard.controller;
+
+
+import android.app.Service;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+/**
+ * Wifi模块
+ * isReadyWifi() --- 是否支持Wifi
+ * getWifiStatus() --- 获取当前手机的Wifi状态
+ * openWifi() --- 开启Wifi
+ * closeWifi() --- 关闭Wifi
+ */
+public class WifiController {
+    private static final String TAG = "WifiController";
+    private WifiManager mWifiManager;
+
+
+    public WifiController(Context context) {
+        mWifiManager= (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+    }
+
+    //是否支持Wifi
+    public boolean isReadyWifi() {
+        if(mWifiManager !=null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //获取当前手机的Wifi状态
+    public boolean getWifiStatus() {
+        if(isReadyWifi()) {
+            return mWifiManager.isWifiEnabled();
+        } else {
+            return false;
+        }
+    }
+
+    //打开Wifi
+    public boolean openWifi() {
+        if(isReadyWifi() && !getWifiStatus()){
+            mWifiManager.setWifiEnabled(true);
+            Log.d(TAG,"openWifi: ");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //关闭Wifi
+    public boolean closeWifi() {
+        if(isReadyWifi() &&getWifiStatus()) {
+            mWifiManager.setWifiEnabled(false);
+            Log.d(TAG,"closeWifi: ");
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
