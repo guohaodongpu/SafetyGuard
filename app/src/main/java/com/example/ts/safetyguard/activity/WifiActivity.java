01d067172f1146651d6e021e8d6e82c1076244a1
yaqing <522675473@qq.com>
2019-04-22 19:04:33 +0800

    Add menu item(wifi)

---
+++ app/src/main/java/com/example/ts/safetyguard/activity/WifiActivity.java
@@ -0,0 +1,16 @@
+package com.example.ts.safetyguard.activity;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ts.safetyguard.R;
+
+public class WifiActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi);
+    }
+}
