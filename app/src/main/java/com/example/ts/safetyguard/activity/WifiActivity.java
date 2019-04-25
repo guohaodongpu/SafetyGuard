2bcb2cc496d0b54ee13d39204f4811e663ab81c6
yaqing <522675473@qq.com>
2019-04-25 14:06:57 +0800

    Finish WifiList

--- app/src/main/java/com/example/ts/safetyguard/activity/WifiActivity.java
+++ app/src/main/java/com/example/ts/safetyguard/activity/WifiActivity.java
@@ -1,11 +1,28 @@
 package com.example.ts.safetyguard.activity;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,42 +30,102 @@ import com.example.ts.safetyguard.R;
 import com.example.ts.safetyguard.adapter.WifiListAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WifiActivity extends AppCompatActivity {
     private ArrayList<String> mWifiList = new ArrayList<String>();
     private WifiListAdapter mWifiAdapter;
     private ListView mListView;
+    private Button mButton;
+    private WifiManager mWifiManager;
+    private List<ScanResult> mScanResults;
+    private ArrayAdapter mAdapter;
+    int PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
+        initListView();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerPermission();
+            }
+        });
+        if(!mWifiManager.isWifiEnabled()) {
+            Toast.makeText(this ,"Wifi",Toast.LENGTH_LONG).show();
+            mWifiManager.setWifiEnabled(true);
+        }
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,mWifiList);
+        mListView.setAdapter(mAdapter);
     }
 
-    private void initData() {
-        mWifiList.clear();
-        for (int i = 0; i < 10; i++){
-            mWifiList.add("Wifi" + i);
-        }
-    }
 
     private void initListView() {
         mListView = findViewById(R.id.wifi_list_view);
+        mButton = findViewById(R.id.scan_button);
         mWifiAdapter = new WifiListAdapter(mWifiList,WifiActivity.this);
-        mListView.setAdapter(mWifiAdapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                String wifiName = mWifiList.get(position);
-                Toast.makeText(WifiActivity.this,"当前点击的是:" + wifiName,
-                        Toast.LENGTH_SHORT).show();
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+    }
+
+    private void scanWifi() {
+        mWifiList.clear();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.wifi.SCAN_RESULTS");
+        intentFilter.addAction("android.net.wifi.WIFI_STATE_CHANGED");
+        registerReceiver(receiver,intentFilter);
+        mWifiManager.startScan();
+        Toast.makeText(this,"Scanning...",Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mScanResults = mWifiManager.getScanResults();
+            Log.d("mScanResults", String.valueOf(mScanResults.size()));
+            unregisterReceiver(this);
+
+            for(ScanResult sr : mScanResults){
+                mWifiList.add(sr.SSID+ "\n" + sr.level + "dBm" );
+                mAdapter.notifyDataSetChanged();
             }
-        });
+        }
+    };
+
+    private void registerPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION },
+                    PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+
+        } else {
+            scanWifi();
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION) {
+            scanWifi();
+        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 }
